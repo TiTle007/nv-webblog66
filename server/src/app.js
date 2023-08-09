b1e4dfccc8f1ede2ae9b45d398f320f3d2a3d9ea
TiTle007 <112480108+TiTle007@users.noreply.github.com>
2023-08-09 05:19:07 +0000

    folder perpared

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+let express = require('express')
+const app = express()
+
+app.get('/status',function(req,res){
+    res.send('Hello nodejs server belong to patipol kaeomuang')
+})
+
+app.get('/hello/:person',function(req,res){
+    console.log('hello - ' + req.params.person)
+    res.send('say hello with ' +req.params.person)
+})
+
+let port = 8081
+app.listen(port, function(){
+    console.log('server running on ' + port)
+})
