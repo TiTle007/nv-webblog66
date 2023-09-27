effd4f9777d33c3bb88e04dee1264db62885fe1a
TiTle007 <tlemoil555@gmail.com>
2023-09-27 14:31:49 +0700

    page 147

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,22 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-const {sequelize} = require('./models')
+let cors = require('cors')
+const { sequelize } = require('./models')
 
 const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extends:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
-require('./rout')(app)
-
-app.get('/status', function (req, res ){
-    res.send('Hello nodejs server 1234567890')
-})
-
-app.get('/hello/:person', function (req,res){
-    console.log('hello - '+ req.params.person);
-    res.send('sey hello with '+req.params.person)
-})
+require('./route')(app)
 
 let port = process.env.PORT || config.port
 
-sequelize.sync({force: false}).then(() => { 
+sequelize.sync({ force: false }).then(() => {
     app.listen(port, function () {
-        console.log('Server running on ' + port) 
+        console.log('Server running on ' + port)
     })
 })
